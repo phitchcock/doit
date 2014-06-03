d87ee32e0ce6984083bffb2d46a76b07b277cb90
phitchcock <phitchcock@me.com>
2014-06-02 22:00:38 -0700

    initial commit, cleaned up gemfile, created lists resource

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.0'
+gem 'sass-rails', '~> 4.0.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+
+# Added Gems
+gem 'bootstrap-sass'
+
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
+
+group :development do
+  gem 'sqlite3'
+  gem 'spring'
+end
+
+group :doc do 
+  gem 'sdoc', '~> 0.4.0'
+end
+
+
+
+
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,57 @@
+class ListsController < ApplicationController
+
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lists = List.all 
+  end
+
+  def show
+  end
+
+  def new 
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: "#{@list.name} was created!"
+    else
+      flash[:error] = "List did not save, please try again"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to @list, notice: "#{@list.name} was updated"
+    else
+      flash[:error] = "List not saved, please try again"
+      render :edit
+    end
+  end
+
+  def destry
+    if @list.destroy
+      redirect_to lists_path, notice: "#{@list.name} was destroyed!"
+    else
+      flash[:error] = "List was not destroyed"
+      redirect_to @list
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit!
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+end

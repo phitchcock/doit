f111ce2774fe368a8a3dfb85d8d3b60ea6f3efa0
phitchcock <phitchcock@me.com>
2014-06-04 10:28:07 -0700

    finished setting up devise invitations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,9 @@ class UsersController < ApplicationController
     @lists = @user.lists
   end
 
+  def invite_user
+    @user = User.invite!(:email => params[:user][:email])
+    render :json => @user
+  end
+
 end
